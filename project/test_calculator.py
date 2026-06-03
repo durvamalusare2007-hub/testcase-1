a0100ae77c070aacbba29f370ac6d40d82e80fbf
STA-MADH-57 <durvamalusare2007@gmail.com>
2026-06-03 10:49:36 +0530

    Added yml test

--- project/test_calculator.py
+++ project/test_calculator.py
@@ -3,4 +3,8 @@ from calculator import add
 def test_add():
     assert add(4, 5) == 9
     assert add(-1, 1) == 0
-    
+
+def test_sub():
+    from calculator import subtract
+    assert subtract(10, 5) == 5
+    assert subtract(0, 1) == -1
